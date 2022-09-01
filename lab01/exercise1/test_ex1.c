c0599d0d774a27f195899aa673f912ee2f8e9d90
Gatsby Huang <gatsby.junce@gmail.com>
2022-09-01 19:53:37 +0800

    Finished lab01

--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -1,43 +1,57 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "ex1.h"
 
-int main(int argc, char **argv) {
-    printf("Running tests...\n\n");
-    /************ Part 1 Test Cases ************/
-    char *str = "hello world!";
+int main(int argc, char** argv) {
+  printf("Running tests...\n\n");
+  /************ Part 1 Test Cases ************/
+  char* str1 = "hello world!";
 
-    int num_l = num_occurrences(str, 'l');
-    assert(num_l == 3);
+  int num_l = num_occurrences(str1, 'l');
+  assert(num_l == 3);
 
-    int num_z = num_occurrences(str, 'z');
-    assert(num_z == 0);
+  int num_z = num_occurrences(str1, 'z');
+  assert(num_z == 0);
 
-    /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+  char* str2 = "";
 
-    printf("Congrats! If you have made it to this line, your Part 1 Test cases are all passing!\n");
+  int num_null = num_occurrences(str2, ' ');
+  assert(num_null == 0);
 
-    /************ Part 2 Test Cases ************/
-    DNA_sequence dna_seq_1;
-    strcpy(dna_seq_1.sequence, "ACTTTGAAC");
-    compute_nucleotide_occurrences(&dna_seq_1);
-    assert(dna_seq_1.A_count == 3);
-    assert(dna_seq_1.C_count == 2);
-    assert(dna_seq_1.G_count == 1);
-    assert(dna_seq_1.T_count == 3);
+  printf(
+      "Congrats! If you have made it to this line, your Part 1 Test cases are "
+      "all passing!\n");
 
-    DNA_sequence dna_seq_2;
-    strcpy(dna_seq_2.sequence, "AAAACCC");
-    compute_nucleotide_occurrences(&dna_seq_2);
-    assert(dna_seq_2.A_count == 4);
-    assert(dna_seq_2.C_count == 3);
-    assert(dna_seq_2.G_count == 0);
-    assert(dna_seq_2.T_count == 0);
+  /************ Part 2 Test Cases ************/
+  DNA_sequence dna_seq_1;
+  strcpy(dna_seq_1.sequence, "ACTTTGAAC");
+  compute_nucleotide_occurrences(&dna_seq_1);
+  assert(dna_seq_1.A_count == 3);
+  assert(dna_seq_1.C_count == 2);
+  assert(dna_seq_1.G_count == 1);
+  assert(dna_seq_1.T_count == 3);
 
-    /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+  DNA_sequence dna_seq_2;
+  strcpy(dna_seq_2.sequence, "AAAACCC");
+  compute_nucleotide_occurrences(&dna_seq_2);
+  assert(dna_seq_2.A_count == 4);
+  assert(dna_seq_2.C_count == 3);
+  assert(dna_seq_2.G_count == 0);
+  assert(dna_seq_2.T_count == 0);
 
-    printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
+  DNA_sequence dna_seq_3;
+  strcpy(dna_seq_3.sequence, "");
+  compute_nucleotide_occurrences(&dna_seq_3);
+  assert(dna_seq_3.A_count == 0);
+  assert(dna_seq_3.C_count == 0);
+  assert(dna_seq_3.G_count == 0);
+  assert(dna_seq_3.T_count == 0);
 
-    return 0;
+  printf(
+      "Congrats! If you have made it to this line, your Part 2 Test cases are "
+      "all passing!\n");
+
+  return 0;
 }
--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -1,6 +1,17 @@
-#include <stddef.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+#include <stddef.h>
+
+int ll_has_cycle(node* head) {
+  node* fast_ptr = head;
+  node* slow_ptr = head;
+
+  do {
+    if (fast_ptr != NULL && fast_ptr->next != NULL)
+      fast_ptr = fast_ptr->next->next;
+    else
+      return 0;
+    slow_ptr = slow_ptr->next;
+  } while (fast_ptr != slow_ptr);
+  return 1;
 }
