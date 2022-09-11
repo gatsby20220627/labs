1542cbaaaa08e5bff867d135a7ce1a6b89eb4c99
Gatsby Huang <gatsby.junce@gmail.com>
2022-09-11 21:09:09 +0800

    Finished Lab 02: C Memory Management, Valgrind

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,19 +1,44 @@
-#include <stdio.h>
 #include "bit_ops.h"
+
+#include <stdio.h>
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+  /*
+  loc         :       n
+  x           :   ????a???
+  loc         :          n
+  x >> n      :   ???????a
+
+  1           :   00000001
+  x >> n & 1  :   0000000a
+  */
+  return x >> n & 1;
 }
 
-/* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
-void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+/* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1
+ */
+void set_bit(unsigned* x, unsigned n, unsigned v) {
+  /*
+  loc:              :       n
+  1 << n            :   00001000
+  ~(1 << n)         :   11110111
+  x &= ~(1 << n)    :   ????0???
+
+  (v << n)          :   0000v000
+  x |= (v << n)     :   ????v???
+  */
+  *x = (*x & ~(1 << n)) | (v << n);
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
-void flip_bit(unsigned *x, unsigned n) {
-    /* YOUR CODE HERE */
-}
+void flip_bit(unsigned* x, unsigned n) {
+  /*
+  loc               :       n
+  x                 :   ????v???
+  1 << n            :   00001000
 
+  x ^ (1 << n)      :   ????^???
+  */
+  *x ^= (1 << n);
+}
--- lab02/ll_cycle.c
+++ lab02/ll_cycle.c
@@ -1,15 +1,16 @@
-#include <stddef.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
-   node *tortoise = head;
-   node *hare = head;                                                                 
-   while (hare != NULL) {
-       tortoise = tortoise->next;
-       hare = hare->next->next;
-       if (tortoise == hare) {
-           return 1;
-       }
-   }
-   return 0;
+#include <stddef.h>
+
+int ll_has_cycle(node* head) {
+  node* tortoise = head;
+  node* hare = head;
+  while (hare != NULL) {
+    if (hare->next == NULL) return 0;
+
+    tortoise = tortoise->next;
+    hare = hare->next->next;
+    if (tortoise == hare) return 1;
+  }
+  return 0;
 }
