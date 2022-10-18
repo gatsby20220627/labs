65281dd93097ff9bb8094d0757c733b9f3c2928f
gatsby <gatsby.huang@gmail.com>
2022-10-18 15:30:11 +0800

    Finished lab03

--- lab03/discrete_fn.s
+++ lab03/discrete_fn.s
@@ -5,7 +5,10 @@
 # a1 is the address of the "output" array (defined above).
 # The return value should be stored in a0
 f:
-
+    addi a0, a0, 3      # a0 += 3
+    slli a0, a0, 2      # a0 <<= 2
+    add a1, a1, a0      # a1 = a0 + a1 (address)
+    lw a0, 0(a1)        # load word
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra  
--- lab03/factorial.s
+++ lab03/factorial.s
@@ -27,7 +27,15 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    add     t1, a0, x0      # t1 -> n
+    addi    t0, x0, 1       # t0 -> i   , init is 1
+    addi    a0, x0, 1       # a0 -> res , init is 1
+    j       done
+loop:
+    mul     a0, a0, t0      # res *= i
+    addi    t0, t0, 1       # i++
+done:
+    bge     t1, t0, loop    # n >= i
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
